e0193bd45a254cdfa74a13f0aa823d2e0223dedc
michito <hienlkt2015@gmail.com>
2025-07-14 23:07:58 +0700

    add init for useful_tools

--- useful_tools/__init__.py
+++ useful_tools/__init__.py
@@ -1,2 +1,6 @@
 # Add read_tfstate to the exports if not already included
 from .read_tfstate_tool import read_tfstate
+
+from .aws_documentation import aws_documentation_search
+from .terraform_documentation import terraform_documentation_search
+
